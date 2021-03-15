063da6cb5e844629d9aec3af057141425d722912
Katerina <harutakaterina@yahoo.com>
2021-03-15 21:31:27 +0200

    primul exercitiu JS

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//am setat constanta pentru kelvin
+//am setat  variabila constanta pentru kelvin
 const kelvin = 293;
-// am setat constanta pentru celsius
+// am setat  variabila constanta pentru celsius
 const celsius = kelvin - 273;
 //am convertit in fahrenheit
-let fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9 / 5) + 32;
 //rotungesc
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
