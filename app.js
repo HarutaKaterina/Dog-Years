80dd5a4ddf9c74ab0a167139b2f79fcd76791a47
Katerina <harutakaterina@yahoo.com>
2021-03-17 15:34:52 +0200

    al doilea exercitiu in JS

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//am setat  variabila constant pentru kelvin
-const kelvin = 293;
-// am setat  variabila constant pentru celsius
-const celsius = kelvin - 273;
-//am convertit in Fahrenheit
-let fahrenheit = celsius * (9 / 5) + 32;
-//rotungesc
-fahrenheit = Math.floor(fahrenheit);
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+const myAge = 33; // am setat variabila const cu varsta mea
+let earlyYears = 2; // am setat variabila let cu primii 2 ani
+earlyYears *= 10.5; // inmultim primii 2 ani la 10.5
+let laterYears = myAge - 2; //am setat variabila din care aflam anii urmatori
+laterYears *= 4;// inmultim anii (31) la 4
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;// aflam anii mei dog years
+let myName = 'Katerina'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years wich is ${myAgeInDogYears} years old in dog years.`);
